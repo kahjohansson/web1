bd4736ed32891a13575224abd1da3b3dab2b2a76
Karina Johansson <kahjohansson@gmail.com>
2021-09-20 17:13:46 -0300

    melhorias no DAO

--- Projeto_1/src/main/java/br/ufscar/dc/dsw/DAO/ClienteDAO.java
+++ Projeto_1/src/main/java/br/ufscar/dc/dsw/DAO/ClienteDAO.java
@@ -2,46 +2,187 @@ package br.ufscar.dc.dsw.DAO;
 
 import br.ufscar.dc.dsw.POJO.Cliente;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Date;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.ArrayList;
 
-public class ClienteDAO {
+public class ClienteDAO extends DAO {
 	
 	public ClienteDAO() {
 		
 	}
 	
-	public void insere(Cliente cliente) {
+	public void insert(Cliente cliente) {
 
-        String sql = "INSERT INTO Cliente (cpf, nome, email, senha, telefone, sexo, dataNasc) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        String query_1 = "INSERT INTO usuarios (cpf, nome, email, senha) VALUES (?, ?, ?, ?)";
+        String query_2 = "INSERT INTO clientes (cpf, sexo, telefone, data_nascimento) VALUES (?, ?, ?, ?)";
 
         try {
-        	// Class.forName("org.apache.derby.jdbc.ClientDriver"); //Derby
-			//String url = "jdbc:derby://localhost:1527/sistema_agendamento"; //Derby
-            Class.forName("com.mysql.cj.jdbc.Driver"); //Mysql
-            String url = "jdbc:mysql://localhost:3306/SistemaAgendamento"; //Mysql
-			Connection conn = (Connection) DriverManager.getConnection(url, "root", "root");
-            PreparedStatement statement = conn.prepareStatement(sql);
+			Connection conn = this.getConnection();
 
-            statement = conn.prepareStatement(sql);
-            statement.setString(1, cliente.getCpf());
-            statement.setString(2, cliente.getNome());
-            statement.setString(3, cliente.getEmail());
-            statement.setString(4, cliente.getSenha());
-            statement.setString(5, cliente.getTelefone());
-            statement.setString(6, cliente.getSexo());
-            statement.setDate(7, java.sql.Date.valueOf(cliente.getDataNasc()));
-            statement.executeUpdate();
+            conn.setAutoCommit(false); // ativa modo de transação
+            
+            PreparedStatement statement_1 = conn.prepareStatement(query_1);
+            statement_1 = conn.prepareStatement(query_1);
+            statement_1.setString(1, cliente.getCpf());
+            statement_1.setString(2, cliente.getNome());
+            statement_1.setString(3, cliente.getEmail());
+            statement_1.setString(4, cliente.getSenha());
+            statement_1.executeUpdate();
+            statement_1.close();
 
-            statement.close();
+            PreparedStatement statement_2 = conn.prepareStatement(query_2);
+            statement_2 = conn.prepareStatement(query_2);
+            statement_2.setString(1, cliente.getCpf());
+            statement_2.setString(2, cliente.getSexo());
+            statement_2.setString(3, cliente.getTelefone());
+            statement_2.setDate(4, Date.valueOf(cliente.getDataNascimento()));
+            statement_2.executeUpdate();
+            statement_2.close();
+
+            conn.commit();
+            conn.setAutoCommit(true);
             conn.close();
             
         } catch (SQLException e) {
-        	System.out.println("O comando SQL não pode ser executado!");
-        } catch (ClassNotFoundException e) {
-        	System.out.println("A classe do driver de conexão não foi encontrada!");
-		}
+        	throw new RuntimeException(e);
+        }
+    }
+
+    public List<Cliente> selectAll() {
+
+        List<Cliente> listaClientes = new ArrayList<>();
+
+        String query = "SELECT * FROM clientes INNER JOIN usuarios ON clientes.cpf = usuarios.cpf";
+
+        try {
+            Connection conn = this.getConnection();
+            Statement statement = conn.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(query);
+            
+            while (resultSet.next()) {
+                
+            	String cpf = resultSet.getString("cpf");
+                String nome = resultSet.getString("nome");
+                String email = resultSet.getString("email");
+                String senha = resultSet.getString("senha");
+                String sexo = resultSet.getString("sexo");
+                String telefone = resultSet.getString("telefone");
+                String data_nascimento = resultSet.getDate("data_nascimento").toString();
+         
+                Cliente cliente = new Cliente(cpf, nome, email, senha, telefone, sexo, data_nascimento);
+                
+                listaClientes.add(cliente);
+            }
+
+            resultSet.close();
+            statement.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return listaClientes;
     }
 	
+    public Cliente selectByCpf(String cpf) {
+
+        Cliente cliente = null;
+
+        String sql = "SELECT * FROM clientes WHERE cpf = ? INNER JOIN usuarios ON clientes.cpf = usuarios.cpf";
+
+        try {
+            Connection conn = this.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setString(1, cpf);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String nome = resultSet.getString("nome");
+                String email = resultSet.getString("email");
+                String senha = resultSet.getString("senha");
+                String sexo = resultSet.getString("sexo");
+                String telefone = resultSet.getString("telefone");
+                String data_nascimento = resultSet.getDate("data_nascimento").toString();
+
+                cliente = new Cliente(cpf, nome, email, senha, telefone, sexo, data_nascimento);
+            }
+
+            resultSet.close();
+            statement.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return cliente;
+    }
+
+    public void update(Cliente cliente) {
+
+        String query_1 = "UPDATE usuarios SET nome = ?, email = ?, senha = ? WHERE cpf = ?";
+        String query_2 = "UPDATE clientes SET telefone = ?, sexo = ?, dataNasc = ? WHERE cpf = ?";
+
+        try {
+            Connection conn = this.getConnection();
+            PreparedStatement statement_1 = conn.prepareStatement(query_1);
+            PreparedStatement statement_2 = conn.prepareStatement(query_2);
+
+            conn.setAutoCommit(false); // ativa modo de transação
+
+            statement_1.setString(1, cliente.getTelefone());
+            statement_1.setString(2, cliente.getSexo());
+            statement_1.setDate(3, java.sql.Date.valueOf(cliente.getDataNascimento()));
+            statement_1.setString(4, cliente.getCpf());
+            statement_1.executeUpdate();
+            statement_1.close();
+         
+            statement_2.setString(1, cliente.getNome());
+            statement_2.setString(2, cliente.getEmail());
+            statement_2.setString(3, cliente.getSenha());
+            statement_2.setString(4, cliente.getCpf());
+            statement_2.executeUpdate();
+            statement_2.close();
+            
+            conn.commit();
+            conn.setAutoCommit(true);
+            conn.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void delete(Cliente cliente) {
+
+        String query_1 = "DELETE FROM usuarios WHERE cpf = ?";
+    	String query_2 = "DELETE FROM clientes WHERE cpf = ?";
+        
+        try {
+            Connection conn = this.getConnection();
+            PreparedStatement statement_1 = conn.prepareStatement(query_1);
+            PreparedStatement statement_2 = conn.prepareStatement(query_2);
+
+            conn.setAutoCommit(false); // ativa modo de transação
+
+            statement_1.setString(1, cliente.getCpf());
+            statement_1.executeUpdate();
+            statement_1.close();
+
+            statement_2.setString(1, cliente.getCpf());
+            statement_2.executeUpdate();
+            statement_2.close();
+            
+            conn.commit();
+            conn.setAutoCommit(true);
+            conn.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
---
+++ Projeto_1/src/main/java/br/ufscar/dc/dsw/DAO/DAO.java
@@ -0,0 +1,26 @@
+package br.ufscar.dc.dsw.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+abstract public class DAO {
+    
+    public DAO() {
+        try {
+        	
+        	Class.forName("com.mysql.cj.jdbc.Driver");
+        	
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected Connection getConnection() throws SQLException {
+    	
+    	String url = "jdbc:mysql://localhost:3306/sistema_agendamento";
+    	
+    	return DriverManager.getConnection(url, "root", "password");
+    }
+
+}
