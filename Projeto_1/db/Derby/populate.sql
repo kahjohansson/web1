a0362356a98cdab857a0d1bb48b68b3b8c24e076
Karina Johansson <kahjohansson@gmail.com>
2021-09-20 11:26:03 -0300

    remoção dos scripts Derby

--- Projeto_1/db/Derby/populate.sql
+++
@@ -1,7 +0,0 @@
-connect 'jdbc:derby:sistema_agendamento;user=root;password=root';
-
-insert into Cliente values ('71079094016', 'André Souza dos Santos', 'andre.souza@email.com', 'senhaandre', '16991234567', 'masculino', '1990-01-01');
-
-disconnect;
-
-quit;
