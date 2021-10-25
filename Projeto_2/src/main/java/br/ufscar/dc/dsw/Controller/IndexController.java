8bda628da1ec68c6d47a0ef19f7a6f3744cbd471
Karina Johansson <kahjohansson@gmail.com>
2021-10-24 21:32:07 -0300

    add index controller and view

---
+++ Projeto_2/src/main/java/br/ufscar/dc/dsw/Controller/IndexController.java
@@ -0,0 +1,20 @@
+package br.ufscar.dc.dsw.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping("")
+	public String index(ModelMap model) {
+		return "index";
+	}
+    
+}
+
