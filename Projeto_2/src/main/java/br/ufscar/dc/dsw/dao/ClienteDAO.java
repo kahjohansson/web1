bc5d0242bef96061b7c1622fa0113702c8f35880
Karina Johansson <kahjohansson@gmail.com>
2021-11-01 19:45:45 -0300

    update daos

--- Projeto_2/src/main/java/br/ufscar/dc/dsw/dao/ClienteDAO.java
+++ Projeto_2/src/main/java/br/ufscar/dc/dsw/dao/ClienteDAO.java
@@ -1,7 +1,6 @@
 package br.ufscar.dc.dsw.dao;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
--- Projeto_2/src/main/java/br/ufscar/dc/dsw/dao/UsuarioDAO.java
+++ Projeto_2/src/main/java/br/ufscar/dc/dsw/dao/UsuarioDAO.java
@@ -1,7 +1,5 @@
 package br.ufscar.dc.dsw.dao;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 import br.ufscar.dc.dsw.domain.Usuario;
 import br.ufscar.dc.dsw.dao.UsuarioDAO;
@@ -9,4 +7,5 @@ import br.ufscar.dc.dsw.dao.UsuarioDAO;
 @SuppressWarnings("unchecked")
 public interface UsuarioDAO extends CrudRepository<Usuario, String>{
 
+    Usuario findByEmail(String email);
 }
