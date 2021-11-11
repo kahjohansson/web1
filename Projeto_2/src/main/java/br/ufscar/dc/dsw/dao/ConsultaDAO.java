a7a5302448fedfb9d1092e659b26c7bec631fb42
Karina Johansson <kahjohansson@gmail.com>
2021-11-11 19:55:18 -0300

    add appointment dao

---
+++ Projeto_2/src/main/java/br/ufscar/dc/dsw/dao/ConsultaDAO.java
@@ -0,0 +1,26 @@
+package br.ufscar.dc.dsw.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.ufscar.dc.dsw.domain.Cliente;
+import br.ufscar.dc.dsw.domain.Consulta;
+import br.ufscar.dc.dsw.domain.Profissional;
+
+@SuppressWarnings("unchecked")
+public interface ConsultaDAO extends CrudRepository<Consulta, Long> {
+
+    Consulta findById(long id);
+	
+	List<Consulta> findAll();
+	
+    List<Consulta> findByCliente(Cliente cliente);
+	
+    List<Consulta> findByProfissional(Profissional profissional);
+    
+    Consulta save(Consulta consulta);
+
+	void deleteById(Long id);
+    
+}
