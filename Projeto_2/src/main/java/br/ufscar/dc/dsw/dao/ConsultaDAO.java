74c660ae63f37316e04c9573f16a4ccb981c61dc
Karina Johansson <kahjohansson@gmail.com>
2021-11-12 08:26:57 -0300

    remove unnecessary comments

--- Projeto_2/src/main/java/br/ufscar/dc/dsw/dao/ConsultaDAO.java
+++ Projeto_2/src/main/java/br/ufscar/dc/dsw/dao/ConsultaDAO.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
-import br.ufscar.dc.dsw.domain.Cliente;
 import br.ufscar.dc.dsw.domain.Consulta;
-import br.ufscar.dc.dsw.domain.Profissional;
 
 @SuppressWarnings("unchecked")
 public interface ConsultaDAO extends CrudRepository<Consulta, Long> {
@@ -14,12 +12,8 @@ public interface ConsultaDAO extends CrudRepository<Consulta, Long> {
     Consulta findById(long id);
 	
 	List<Consulta> findAll();
-	
-    // List<Consulta> findByCliente(Cliente cliente);
 
     List<Consulta> findByCpfCliente(String cpfCliente);
-	
-    // List<Consulta> findByProfissional(Profissional profissional);
 
     List<Consulta> findByCpfProfissional(String cpfProfissional);
     
