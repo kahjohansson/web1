3606f60b9aa27374cf53cc4cf56de3da6b182f45
Karina Johansson <kahjohansson@gmail.com>
2021-11-12 08:18:38 -0300

    add id column in the appointment table

--- Projeto_2/db/mysql/create.sql
+++ Projeto_2/db/mysql/create.sql
@@ -47,12 +47,13 @@ CREATE TABLE IF NOT EXISTS profissionais (
     );
 
 CREATE TABLE IF NOT EXISTS consultas(
+  id INT AUTO_INCREMENT NOT NULL,
 	cpfCliente varchar(11) NOT NULL,
 	cpfProfissional varchar(14) NOT NULL,
 	data DATE NOT NULL,
   horario INT NOT NULL,
-    
-	CONSTRAINT fk_cpf_cliente
+
+  CONSTRAINT fk_cpf_cliente
   FOREIGN KEY (cpfCliente) 
   REFERENCES clientes(cpf) 
   ON DELETE CASCADE,
@@ -61,8 +62,8 @@ CREATE TABLE IF NOT EXISTS consultas(
   FOREIGN KEY (cpfProfissional)
   REFERENCES profissionais(cpf)
   ON DELETE CASCADE,
-
- 	PRIMARY KEY (cpfCliente, cpfProfissional, data)
+    
+ 	PRIMARY KEY (id)
 );
 
 SET GLOBAL time_zone = '-3:00';
